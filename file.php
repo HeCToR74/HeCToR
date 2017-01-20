bafe466b0d132227615674509ecbba010a136d39
HeCToR74 <v.nesterenko@chnu.edu.ua>
2017-01-20 17:49:16 +0300

    Delete file.php

--- file.php
+++
@@ -1,5 +0,0 @@
-<?php
-$f = fopen("base.txt", "a");
-fwrite($f, $_POST["name_flight"]."\t".$_POST["name_company"]."\t".$_POST["pilot_surname"]."\t".$_POST["pilot_name"]."\t".$_POST["city_from"]."\t".$_POST["city_to"]."\t".$_POST["price"]."\t".$_POST["time"]."\t".$_POST["mark"].PHP_EOL); 
-fclose($f);
-?>
