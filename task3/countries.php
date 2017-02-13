5d2b2c724653f685f78d7eaa1bf7ce88e8446b2b
HeCToR74 <v.nesterenko@chnu.edu.ua>
2017-02-13 18:09:18 +0200

    first commit

---
+++ task3/countries.php
@@ -0,0 +1,32 @@
+<?php
+
+class Countries {
+
+	protected $ID_countries;
+	protected $name_countries;
+
+	function__construct() {
+ 		
+ 	}
+
+ 	function openConnection() {
+
+ 	}
+
+ 	function select() {
+
+ 	}
+
+ 	function insert() {
+
+ 	}
+
+ 	function delete() {
+
+ 	}
+
+ 	function closeConnection() {
+ 		
+ 	}
+
+}
